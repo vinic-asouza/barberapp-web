7d68e4aefa0bad3fa5f316a64c06051915d8ec69
Prisma MBP 103 <prisma@Assis.local>
2019-11-18 16:15:28 -0300

    store user

---
+++ src/store/modules/user/sagas.js
@@ -0,0 +1,8 @@
+import { all } from 'redux-saga/effects';
+
+// import history from '~/services/history';
+// import api from '~/services/api';
+
+// import { signInSuccess } from './actions';
+
+export default all([]);
