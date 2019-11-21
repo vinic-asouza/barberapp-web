127fe888313903f5e3c793f2784796e58caffd10
Vinicius Souza <vinic.asouza@gmail.com>
2019-11-20 22:19:52 -0300

    feat: profile page styles

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Form, Input} from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+
+  }
+
   return (
-    <div />
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Email" />
+        <hr/>
+
+        <Input name="oldPassword" type="password" placeholder="Senha atual" />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input name="confirmPassword" type="password" placeholder="Confirmar nova senha" />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container> 
   );
 }
