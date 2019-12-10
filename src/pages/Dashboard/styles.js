419c49ae0a4a6578da3a7d931a247cfa3776b7eb
Vinicius Souza <vinic.asouza@gmail.com>
2019-12-09 22:04:38 -0300

    feat: show schedule in dashboard

--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -3,27 +3,22 @@ import styled from 'styled-components';
 export const Container = styled.div`
   max-width: 600px;
   margin: 50px auto;
-
   display: flex;
   flex-direction: column;
-
   header {
     display: flex;
-    align-self: center;
     align-items: center;
-
+    align-self: center;
     button {
       border: 0;
       background: none;
     }
-
     strong {
       color: #fff;
       font-size: 24px;
       margin: 0 15px;
     }
   }
-
   ul {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
@@ -32,20 +27,17 @@ export const Container = styled.div`
   }
 `;
 
-export const Time = styled.div`
+export const Time = styled.li`
   padding: 20px;
   border-radius: 4px;
-  background: #FFF;
-
-  opacity: ${props => (props.past ? 0.6 : 1)}
-
+  background: #fff;
+  opacity: ${props => (props.past ? 0.6 : 1)};
   strong {
     display: block;
     color: ${props => (props.available ? '#999' : '#7159c1')};
     font-size: 20px;
     font-weight: normal;
   }
-
   span {
     display: block;
     margin-top: 3px;
